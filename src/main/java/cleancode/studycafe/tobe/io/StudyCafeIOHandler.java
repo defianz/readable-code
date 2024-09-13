edc163a54f2dd59cb05f24700c1400bcb9bac029
defianz <iautm91@gmail.com>
2024-09-13 16:40:08 +0900

    [6-3] 리팩토링(2) - IO 통합

--- src/main/java/cleancode/studycafe/tobe/io/StudyCafeIOHandler.java
+++ src/main/java/cleancode/studycafe/tobe/io/StudyCafeIOHandler.java
@@ -2,34 +2,23 @@ package cleancode.studycafe.tobe.io;
 
 import cleancode.studycafe.tobe.model.StudyCafeLockerPass;
 import cleancode.studycafe.tobe.model.StudyCafePass;
+import cleancode.studycafe.tobe.model.StudyCafePassType;
 
 import java.util.List;
 
-public class OutputHandler {
+public class StudyCafeIOHandler {
 
-    public void askPassTypeSelection() {
-        System.out.println("사용하실 이용권을 선택해 주세요.");
-        System.out.println("1. 시간 이용권(자유석) | 2. 주단위 이용권(자유석) | 3. 1인 고정석");
+    private final InputHandler inputHandler = new InputHandler();
+    private final OutputHandler outputHandler = new OutputHandler();
+
+    public void showWelcomeMessage() {
+        System.out.println("*** 프리미엄 스터디카페 ***");
     }
 
-    public void showPassListForSelection(List<StudyCafePass> passes) {
+    public void showAnnouncement() {
+        System.out.println("* 사물함은 고정석 선택 시 이용 가능합니다. (추가 결제)");
+        System.out.println("* !오픈 이벤트! 2주권 이상 결제 시 10% 할인, 12주권 결제 시 15% 할인! (결제 시 적용)");
         System.out.println();
-        System.out.println("이용권 목록");
-        for (int index = 0; index < passes.size(); index++) {
-            StudyCafePass pass = passes.get(index);
-            System.out.println(String.format("%s. ", index + 1) + pass.display());
-        }
-    }
-
-    public void askLockerPass(StudyCafeLockerPass lockerPass) {
-        System.out.println();
-        String askMessage = String.format(
-                "사물함을 이용하시겠습니까? (%s)",
-                lockerPass.display()
-        );
-
-        System.out.println(askMessage);
-        System.out.println("1. 예 | 2. 아니오");
     }
 
     public void showPassOrderSummary(StudyCafePass selectedPass) {
@@ -59,4 +48,18 @@ public class OutputHandler {
         System.out.println(message);
     }
 
+    public StudyCafePassType askPassTypeSelecting() {
+        outputHandler.askPassTypeSelection();
+        return inputHandler.getPassTypeSelectingUserAction();
+    }
+
+    public StudyCafePass askPassSelecting(List<StudyCafePass> passCandidates) {
+        outputHandler.showPassListForSelection(passCandidates);
+        return inputHandler.getSelectPass(passCandidates);
+    }
+
+    public boolean askLockerPass(StudyCafeLockerPass lockerPassCandidate) {
+        outputHandler.askLockerPass(lockerPassCandidate);
+        return inputHandler.getLockerSelection();
+    }
 }
